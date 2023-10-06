f85506db8364ee91f9720590fdc3b6251e8f4e45
NoahVW <136670476+NoahVW@users.noreply.github.com>
2023-10-06 07:42:59 -0500

    client structure2

--- code/Client.java
+++ code/Client.java
@@ -1,16 +1,29 @@
 public class Client{
 
     public static void main(String[] args){
-        getKeys(targetPublicKey)             //Retrieve the target groups public keys
+        int getKeys;
+        
+        public void getKeys(int targetPublicKey){           //Retrieve the target groups public keys
+        }             
        
-        setKeys(e, n)                        //Use randomly generated values to initiate create and set clients keys for the session
-       
-        connect(host, port)                  //Connect to the server on the serving port 
-       
-        newMessage(groupNum)                 //Should be checked for in a loop. Message should be encrypted using the targets public keys and sent to server with the clients group number 
-       
-        exitSession                          //Used to exit out of the server by each Client.  
+        public void setKeys(int e, n){                      //Use randomly generated values to initiate create and set clients keys for the session
 
+        }                        
+       
+        public void connect(int host, port){                //Connect to the server on the serving port
+
+
+        }                   
+       
+        public void newMessage(int groupNum){               //Should be checked for in a loop. Message should be encrypted using the targets public keys and sent to server with the clients group number 
+
+
+        }                 
+       
+        public void exitSession(int exit){                  //Used to exit out of the server by each Client.  
+
+
+        }                     
 
 
 
